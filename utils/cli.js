ee26d3247f87d7b63cdcecf18b2e6a882256afd7
Sukriti-sood <soodsukriti1111@gmail.com>
2021-10-08 10:18:21 +0530

    updated documentation

--- utils/cli.js
+++ utils/cli.js
@@ -2,68 +2,69 @@ const meow = require('meow');
 const meowHelp = require('cli-meow-help');
 
 const flags = {
-	id: {
-		type: `string`,
-		alias: `i`,
-		desc: `Fetches input and output using only the contest ID`
-	},
-	url: {
-		type: `string`,
-		alias: `u`,
-		desc: `Fetches sample inputs and outputs and creates folder for the contest`,
-		default: ``
-	},
-	folder: {
-		type: `string`,
-		alias: `f`,
-		desc: `Defines the directory name for the folder in which the files will be`,
-		default: ``
-	},
-	language: {
-		type: `string`,
-		alias: `l`,
-		desc: `Preferred language to write files. For example '-l cpp' will create .cpp files to write solutions`,
-		default: ``
-	},
-	clear: {
-		type: `boolean`,
-		default: false,
-		alias: `c`,
-		desc: `Clear the console`
-	},
-	noClear: {
-		type: `boolean`,
-		default: false,
-		desc: `Don't clear the console`
-	},
-	debug: {
-		type: `boolean`,
-		default: false,
-		alias: `d`,
-		desc: `Print debug info`
-	},
-	version: {
-		type: `boolean`,
-		alias: `v`,
-		desc: `Print CLI version`
-	}
+    id: {
+        type: `string`,
+        alias: `i`,
+        desc: `Fetches input and output using only the contest ID`,
+        default: ''
+    },
+    url: {
+        type: `string`,
+        alias: `u`,
+        desc: `Fetches sample inputs and outputs and creates folder for the contest`,
+        default: ``
+    },
+    folder: {
+        type: `string`,
+        alias: `f`,
+        desc: `Defines the directory name for the folder in which the files will be`,
+        default: ``
+    },
+    language: {
+        type: `string`,
+        alias: `l`,
+        desc: `Preferred language to write files. For example '-l cpp' will create .cpp files to write solutions`,
+        default: ``
+    },
+    clear: {
+        type: `boolean`,
+        default: false,
+        alias: `c`,
+        desc: `Clear the console`
+    },
+    noClear: {
+        type: `boolean`,
+        default: false,
+        desc: `Don't clear the console`
+    },
+    debug: {
+        type: `boolean`,
+        default: false,
+        alias: `d`,
+        desc: `Print debug info`
+    },
+    version: {
+        type: `boolean`,
+        alias: `v`,
+        desc: `Print CLI version`
+    }
 };
 
 const commands = {
-	help: { desc: `Print help info` }
+    help: { desc: `Print help info` }
 };
 
 const helpText = meowHelp({
-	name: `codeforces-contest`,
-	flags,
-	commands
+    name: `codeforces-contest`,
+    flags,
+    commands
 });
 
 const options = {
-	inferType: true,
-	description: false,
-	hardRejection: false,
-	flags
+    inferType: true,
+    description: false,
+    hardRejection: false,
+    flags
 };
 
 module.exports = meow(helpText, options);
--- utils/fetchIO.js
+++ utils/fetchIO.js
@@ -10,213 +10,213 @@ const randomColors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan'];
 let spinner;
 
 function askQuestion(query) {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-	return new Promise(resolve =>
-		rl.question(query, ans => {
-			rl.close();
-			resolve(ans);
-		})
-	);
+    return new Promise(resolve =>
+        rl.question(query, ans => {
+            rl.close();
+            resolve(ans);
+        })
+    );
 }
 
 const getProblems = async codeforcesLink => {
-	try {
-		const { data } = await axios.get(codeforcesLink);
+    try {
+        const { data } = await axios.get(codeforcesLink);
 
-		const $ = cheerio.load(data);
-		const links = $(`table.problems a`).toArray();
-		const array = links.filter(
-			link =>
-				link.children &&
-				link.children[0].type === 'comment' &&
-				link.children[0].next.type === 'text'
-		);
-		const titles = array
-			.filter(
-				link =>
-					link.children &&
-					link.children[0].type === 'comment' &&
-					link.children[0].next.type === 'text'
-			)
-			.map(link => link.children[0].next.data);
-		const problemLinks = array.map(link => link.attribs.href);
+        const $ = cheerio.load(data);
+        const links = $(`table.problems a`).toArray();
+        const array = links.filter(
+            link =>
+            link.children &&
+            link.children[0].type === 'comment' &&
+            link.children[0].next.type === 'text'
+        );
+        const titles = array
+            .filter(
+                link =>
+                link.children &&
+                link.children[0].type === 'comment' &&
+                link.children[0].next.type === 'text'
+            )
+            .map(link => link.children[0].next.data);
+        const problemLinks = array.map(link => link.attribs.href);
 
-		return { problemLinks, titles };
-	} catch (error) {
-		console.error('Some Error Occurred', error.message);
-		process.exit(1);
-	}
+        return { problemLinks, titles };
+    } catch (error) {
+        console.error('Some Error Occurred', error.message);
+        process.exit(1);
+    }
 };
 
-const createProblemSubFolders = async (
-	problemLinks = [],
-	folderPath,
-	language = '',
-	titles = []
+const createProblemSubFolders = async(
+    problemLinks = [],
+    folderPath,
+    language = '',
+    titles = []
 ) => {
-	if (!problemLinks.length) {
-		console.log('No Links Available');
-		process.exit(1);
-	}
+    if (!problemLinks.length) {
+        console.log('No Links Available');
+        process.exit(1);
+    }
 
-	try {
-		for (let i = 0; i < problemLinks.length; i++) {
-			// Getting Title
-			const title = titles[i];
-			spinner.start(`Fetching ${title}...`);
+    try {
+        for (let i = 0; i < problemLinks.length; i++) {
+            // Getting Title
+            const title = titles[i];
+            spinner.start(`Fetching ${title}...`);
 
-			spinner.color = randomColors[i % (randomColors.length + 1)];
+            spinner.color = randomColors[i % (randomColors.length + 1)];
 
-			const link = problemLinks[i];
-			const problemId = link.split('/').slice(-1)[0];
+            const link = problemLinks[i];
+            const problemId = link.split('/').slice(-1)[0];
 
-			const { data } = await axios.get(`https://codeforces.com${link}`);
+            const { data } = await axios.get(`https://codeforces.com${link}`);
 
-			const $ = cheerio.load(data);
+            const $ = cheerio.load(data);
 
-			// Getting Inputs
-			const inputs = $('div.sample-test > div.input > pre')
-				.toArray()
-				.map(input => input.children[0]?.data);
+            // Getting Inputs
+            const inputs = $('div.sample-test > div.input > pre')
+                .toArray()
+                .map(input => input.children[0] ? .data);
 
-			// Getting Outputs
-			const outputs = $('div.sample-test > div.output > pre')
-				.toArray()
-				.map(output => output.children[0]?.data);
+            // Getting Outputs
+            const outputs = $('div.sample-test > div.output > pre')
+                .toArray()
+                .map(output => output.children[0] ? .data);
 
-			// making subFolder
+            // making subFolder
 
-			const subFolderPath = path.join(folderPath, problemId);
-			fs.mkdirSync(subFolderPath);
+            const subFolderPath = path.join(folderPath, problemId);
+            fs.mkdirSync(subFolderPath);
 
-			// making sample input files
-			inputs.forEach((input, idx) => {
-				fs.writeFileSync(
-					path.join(subFolderPath, `in_${idx + 1}.txt`),
-					input
-				);
-			});
+            // making sample input files
+            inputs.forEach((input, idx) => {
+                fs.writeFileSync(
+                    path.join(subFolderPath, `in_${idx + 1}.txt`),
+                    input
+                );
+            });
 
-			// making sample output files
-			outputs.forEach((output, idx) => {
-				fs.writeFileSync(
-					path.join(subFolderPath, `out_${idx + 1}.txt`),
-					output
-				);
-			});
+            // making sample output files
+            outputs.forEach((output, idx) => {
+                fs.writeFileSync(
+                    path.join(subFolderPath, `out_${idx + 1}.txt`),
+                    output
+                );
+            });
 
-			// make programfiles
+            // make programfiles
 
-			if (language.length) {
-				fs.writeFileSync(
-					path.join(
-						subFolderPath,
-						`${problemId.toLowerCase()}.${language}`
-					),
-					''
-				);
-			}
-			spinner.succeed(`Fetched ${title}`);
-		}
-	} catch (error) {
-		spinner.fail('Failed');
-		process.exit(1);
-	}
+            if (language.length) {
+                fs.writeFileSync(
+                    path.join(
+                        subFolderPath,
+                        `${problemId.toLowerCase()}.${language}`
+                    ),
+                    ''
+                );
+            }
+            spinner.succeed(`Fetched ${title}`);
+        }
+    } catch (error) {
+        spinner.fail('Failed');
+        process.exit(1);
+    }
 };
 
 module.exports = async function fetchIO(
-	url = '',
-	id = '',
-	folderName = '',
-	language = ''
+    url = '',
+    id = '',
+    folderName = '',
+    language = ''
 ) {
-	const ora = await (await import('ora')).default;
-	spinner = ora('');
-	const codeforcesLink = id.length
-		? `https://codeforces.com/contest/${id}`
-		: url;
+    const ora = await (await
+        import ('ora')).default;
+    spinner = ora('');
+    const codeforcesLink = id.length ?
+        `https://codeforces.com/contest/${id}` :
+        url;
 
-	// checking if the url is valid
-	const re = /https:\/\/codeforces\.com\/contest\/[0-9]+/i;
-	if (codeforcesLink.length > 0 && !re.test(codeforcesLink)) {
-		alert({
-			type: `error`,
-			name: `Invalid arguments`,
-			msg: url
-				? `The url entered is invalid`
-				: `Contest ID entered is invalid`
-		});
-		process.exit(1);
-	}
+    // checking if the url is valid
+    const re = /https:\/\/codeforces\.com\/contest\/[0-9]+/i;
+    if (codeforcesLink.length > 0 && !re.test(codeforcesLink)) {
+        alert({
+            type: `error`,
+            name: `Invalid arguments`,
+            msg: url ?
+                `The url entered is invalid` : `Contest ID entered is invalid`
+        });
+        process.exit(1);
+    }
 
-	const contestId = url.length ? codeforcesLink.split('/').slice(-1)[0] : id;
-	const { problemLinks, titles } = await getProblems(codeforcesLink);
-	if (!problemLinks.length) {
-		alert({
-			type: `error`,
-			name: `No links found`,
-			msg: `No links were found for the given contest`
-		});
-		alert({
-			type: `info`,
-			name: `Give a star`,
-			msg: `https://github.com/TheNinza/codeforces-contest`
-		});
-		process.exit(1);
-	}
+    const contestId = url.length ? codeforcesLink.split('/').slice(-1)[0] : id;
+    const { problemLinks, titles } = await getProblems(codeforcesLink);
+    if (!problemLinks.length) {
+        alert({
+            type: `error`,
+            name: `No links found`,
+            msg: `No links were found for the given contest`
+        });
+        alert({
+            type: `info`,
+            name: `Give a star`,
+            msg: `https://github.com/TheNinza/codeforces-contest`
+        });
+        process.exit(1);
+    }
 
-	if (problemLinks?.length) {
-		const contestFolderPath = path.join(
-			process.cwd(),
-			folderName ? folderName : contestId
-		);
+    if (problemLinks ? .length) {
+        const contestFolderPath = path.join(
+            process.cwd(),
+            folderName ? folderName : contestId
+        );
 
-		// checking if folder already exists
-		if (fs.existsSync(contestFolderPath)) {
-			alert({
-				type: `warning`,
-				name: `Folder Already Exists`,
-				msg: `Do you want to remove the folder '${
+        // checking if folder already exists
+        if (fs.existsSync(contestFolderPath)) {
+            alert({
+                type: `warning`,
+                name: `Folder Already Exists`,
+                msg: `Do you want to remove the folder '${
 					folderName ? folderName : contestId
 				}'? \n(y)es/(n)o ENTER`
-			});
+            });
 
-			const ans = await askQuestion(``);
+            const ans = await askQuestion(``);
 
-			if (ans.toLowerCase() === 'y' || ans.toLowerCase() === 'yes') {
-				fs.rmSync(contestFolderPath, { recursive: true, force: true });
-			} else {
-				return;
-			}
-		}
-		// Create contest folder
-		console.log('\nCreating Folder\n');
+            if (ans.toLowerCase() === 'y' || ans.toLowerCase() === 'yes') {
+                fs.rmSync(contestFolderPath, { recursive: true, force: true });
+            } else {
+                return;
+            }
+        }
+        // Create contest folder
+        console.log('\nCreating Folder\n');
 
-		fs.mkdirSync(contestFolderPath);
+        fs.mkdirSync(contestFolderPath);
 
-		// creating subFolder for each problem
-		await createProblemSubFolders(
-			problemLinks,
-			contestFolderPath,
-			language,
-			titles
-		);
-	}
-	spinner.stop();
+        // creating subFolder for each problem
+        await createProblemSubFolders(
+            problemLinks,
+            contestFolderPath,
+            language,
+            titles
+        );
+    }
+    spinner.stop();
 
-	alert({
-		type: `success`,
-		name: `Completed`,
-		msg: `Created folder '${folderName ? folderName : contestId}'`
-	});
+    alert({
+        type: `success`,
+        name: `Completed`,
+        msg: `Created folder '${folderName ? folderName : contestId}'`
+    });
 
-	alert({
-		type: `info`,
-		name: `Give a star`,
-		msg: `https://github.com/TheNinza/codeforces-contest`
-	});
+    alert({
+        type: `info`,
+        name: `Give a star`,
+        msg: `https://github.com/TheNinza/codeforces-contest`
+    });
 };
